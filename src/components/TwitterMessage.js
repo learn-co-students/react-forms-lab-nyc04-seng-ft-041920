c6dafbffd3829b273059be76569daabe073898c6
“dinadeljanin” <ddeljanin5@icloud.com>
2020-07-14 11:42:17 -0400

    Done.

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,17 +4,32 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      // empty string
+      message: ""
+    };
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input
+          type="text"
+          name="message"
+          id="message"
+          onChange={event => this.setState({
+            message: event.target.value
+          })}
+          value={this.state.message}/>
+          {/* console.log(this.state.message.length) */}
+          <br/>
+          {this.props.maxChars - this.state.message.length}
       </div>
     );
   }
 }
 
 export default TwitterMessage;
+
+// This component takes one prop: maxChars which is a number — the maximum amount of characters a message can have. This prop is being passed in from the App component with the value 280.
